9ffa3449e96aebb9a8e0b9f57d8500ab86fe0e0f
shengann <shengann2412@gmail.com>
2023-02-12 16:17:35 +0800

    create models for finacial ratios & Stock

--- StockProf_Services/StockProf_app/admin.py
+++ StockProf_Services/StockProf_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from StockProf_app.models import stock, financialRatios
 
 # Register your models here.
+admin.site.register(stock)
+admin.site.register(financialRatios)
--- StockProf_Services/StockProf_app/models.py
+++ StockProf_Services/StockProf_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class stock (models.Model):
+    name = models.CharField(max_length=50)
+    ticket = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.name
+    
+class financialRatios (models.Model):
+    ticket = models.ForeignKey(stock, on_delete=models.CASCADE, related_name='financialRatios')
+    assetturnover= models.IntegerField()
+    quickratio = models.IntegerField()
+    debttoequity = models.IntegerField()
+    roe = models.IntegerField()
+    dividendyield = models.IntegerField()
+    pricetoearnings = models.IntegerField()
+    created = models.DateField(auto_now=False, auto_now_add=False)
