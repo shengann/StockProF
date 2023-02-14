0d3788c92ba771d91b5ab13dafffcdd86c60f434
shengann <shengann2412@gmail.com>
2023-02-14 10:35:37 +0800

    create stock & financial Ratio Model

--- StockProf_Services/StockProf_app/models.py
+++ StockProf_Services/StockProf_app/models.py
@@ -1,19 +1,33 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 class stock (models.Model):
-    name = models.CharField(max_length=50)
-    ticket = models.CharField(max_length=20)
+    Symbol = models.CharField(max_length=50,unique=True)
+    AssetType = models.CharField(max_length=20)
+    Name = models.CharField(max_length=220)
+    CIK = models.CharField(max_length=20, null=True, blank=True)
+    Exchange = models.CharField(max_length=20)
+    Currency = models.CharField(max_length=20)
+    Country = models.CharField(max_length=20)
+    Sector = models.CharField(max_length=20)
+    Industry = models.CharField(max_length=220)
+    Address = models.CharField(max_length=220,null=True, blank=True)
     
     def __str__(self):
-        return self.name
+        return str(self.Symbol) + "  " + str(self.Name)
     
 class financialRatios (models.Model):
-    ticket = models.ForeignKey(stock, on_delete=models.CASCADE, related_name='financialRatios')
-    assetturnover= models.IntegerField()
-    quickratio = models.IntegerField()
-    debttoequity = models.IntegerField()
-    roe = models.IntegerField()
-    dividendyield = models.IntegerField()
-    pricetoearnings = models.IntegerField()
-    created = models.DateField(auto_now=False, auto_now_add=False)
+    ticker = models.ForeignKey(
+        stock, on_delete=models.CASCADE, related_name='financialRatios', to_field='Symbol', null=True, blank=True)
+    # ticket = models.CharField(max_length=20)
+    assetturnover = models.DecimalField(max_digits=6, decimal_places=4)
+    quickratio = models.DecimalField(max_digits=6, decimal_places=4)
+    debttoequity = models.DecimalField(max_digits=6, decimal_places=4)
+    roe = models.DecimalField(max_digits=6, decimal_places=4)
+    dividendyield = models.DecimalField(max_digits=6, decimal_places=4)
+    pricetoearnings = models.DecimalField(max_digits=6, decimal_places=4)
+    date = models.DateField()
+
+    def __str__(self):
+        return str(self.ticker) + "  " +str(self.date)
