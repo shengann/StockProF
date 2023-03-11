0794253fe8b866a87dedc1f6c2250e99548901fc
shengann <shengann2412@gmail.com>
2023-03-12 00:23:15 +0800

    stockList api url

--- StockProf_Services/StockProf_app/api/urls.py
+++ StockProf_Services/StockProf_app/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from StockProf_app.api.views import getStockData, getFinancialRatosData, getStockProfData, getIndustryTicker
+from StockProf_app.api.views import getStockData, getFinancialRatosData, getStockProfData, getIndustryTicker, stockList
 
 router = DefaultRouter()
 urlpatterns = [
+    path('stocks', stockList.as_view(), name='stock-List'),
     path('stock', getStockData.as_view(), name='get-Stock-Data'),
     path('stock/financial_ratio',
          getFinancialRatosData.as_view(), name='getFinancialRatosData'),
