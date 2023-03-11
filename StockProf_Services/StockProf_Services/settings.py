7ec4e7845bcc904dd758be81aacc54ee1054c0e5
shengann <shengann2412@gmail.com>
2023-03-11 22:51:03 +0800

    configure settings.py for web development

--- StockProf_Services/StockProf_Services/settings.py
+++ StockProf_Services/StockProf_Services/settings.py
@@ -38,13 +38,21 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'StockProf_app',
-    'rest_framework'
+    'rest_framework',
+    'rest_framework.authtoken',
+    'corsheader',
+    'djoser'
 
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://172.16.136.253:8080",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
