3cb7f0906c5a5349107ba4b06cdd6411a737ed90
shengann <shengann2412@gmail.com>
2023-03-12 19:31:35 +0800

    add bootstrap to vue

--- stockprof/src/main.js
+++ stockprof/src/main.js
@@ -1,3 +1,4 @@
+import "bootstrap/dist/css/bootstrap.css"
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -7,3 +8,5 @@ import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8000'
 
 createApp(App).use(store).use(router).mount('#app')
+
+import "bootstrap/dist/js/bootstrap.js"
