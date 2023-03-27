75df16d45dfc7faf79702db37e5371500e43d496
shengann <shengann2412@gmail.com>
2023-03-27 23:11:37 +0800

    added stock_price model

--- StockProf_Services/StockProf_app/models.py
+++ StockProf_Services/StockProf_app/models.py
@@ -52,3 +52,17 @@ class MY_financialRatios (models.Model):
 
     def __str__(self):
         return str(self.ticker) 
+    
+
+class MY_stockPrice (models.Model):
+    ticker = models.ForeignKey(MY_stock, on_delete=models.CASCADE,related_name='stock_price', to_field='Symbol', null=True, blank=True)
+    open = models.DecimalField(max_digits=15, decimal_places=4)
+    high = models.DecimalField(max_digits=15, decimal_places=4)
+    low = models.DecimalField(max_digits=15, decimal_places=4)
+    close = models.DecimalField(max_digits=15, decimal_places=4)
+    volume = models.DecimalField(max_digits=15, decimal_places=4)
+    date = date = models.DateField()
+
+
+    def __str__(self):
+        return str(self.ticker) + "  " + str(self.date)
