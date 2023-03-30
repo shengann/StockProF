b93c08bc8d22ca0b4f30851e5bc7bda9c5fa0f2b
shengann <shengann2412@gmail.com>
2023-03-30 17:36:06 +0800

    delete us stock model

--- StockProf_Services/StockProf_app/models.py
+++ StockProf_Services/StockProf_app/models.py
@@ -2,34 +2,6 @@ from django.db import models
 import datetime
 
 # Create your models here.
-class stock (models.Model):
-    Symbol = models.CharField(max_length=50,unique=True)
-    Name = models.CharField(max_length=220, blank=True)
-    Exchange = models.CharField(max_length=20, blank=True)
-    Sector = models.CharField(max_length=20)
-    Industry = models.CharField(max_length=220, blank=True)
-    
-    def __str__(self):
-        return str(self.Symbol) + "  " + str(self.Name)
-    
-    def get_absolute_url(self):
-        return f'/{self.Symbol}'
-    
-class financialRatios (models.Model):
-    ticker = models.ForeignKey(
-        stock, on_delete=models.CASCADE, related_name='financialRatios', to_field='Symbol', null=True, blank=True)
-    # ticket = models.CharField(max_length=20)
-    assetturnover = models.DecimalField(max_digits=10, decimal_places=4)
-    quickratio = models.DecimalField(max_digits=10, decimal_places=4)
-    debttoequity = models.DecimalField(max_digits=10, decimal_places=4)
-    roe = models.DecimalField(max_digits=10, decimal_places=4)
-    dividendyield = models.DecimalField(max_digits=10, decimal_places=4)
-    pricetoearnings = models.DecimalField(max_digits=10, decimal_places=4)
-    date = models.DateField()
-
-    def __str__(self):
-        return str(self.ticker) + "  " +str(self.date)
-
 class MY_stock (models.Model):
     Symbol = models.CharField(max_length=50,unique=True)
     Name = models.CharField(max_length=220, blank=True)
