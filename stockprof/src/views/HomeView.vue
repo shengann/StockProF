0497c1f74df4807dbc830c6201aa4445abe3dfca
shengann <shengann2412@gmail.com>
2023-03-30 23:34:38 +0800

    feature/financial ratios display

--- stockprof/src/views/HomeView.vue
+++ stockprof/src/views/HomeView.vue
@@ -7,7 +7,7 @@
         type="button" id="dropdownMenuButton1"
         data-bs-toggle="dropdown"
         aria-expanded="false">
-        Dropdown button: {{ value }}
+        Category : {{ value }}
       </button>
       <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1" role="menu">
         <li v-for="option in options" :key="option">
@@ -32,15 +32,27 @@
             <th class="text-center" scope="col">Name</th>
             <th class="text-center" scope="col">Code</th>
             <th class="text-center" scope="col">Category</th>
+            <th class="text-center" scope="col">Total asset turnover</th>
+            <th class="text-center" scope="col">Cash ratio</th>
+            <th class="text-center" scope="col">Debt ratio</th>
+            <th class="text-center" scope="col">Return on equity </th>
+            <th class="text-center" scope="col">Dividend yield</th>
+            <th class="text-center" scope="col">Price earnings ratio </th>
              <th class="text-center" scope="col"></th>
           </tr>
         </thead>
         <tbody>
-          <tr v-for="stock in stocks" :key="stock.id">
+          <tr v-for="(stock, index) in stocks" :key="index">
             <td scope="row">{{ stock.Name }}</td>
             <td scope="row">{{ stock.Symbol }}</td>
             <td scope="row">{{ stock.Category }}</td>
-            <td scope="row"><router-link v-bind:to="stock.get_absolute_url" class="button is-dark mt-4">View details</router-link></td>
+            <td scope="row">{{ financialRatio[index].assetturnover }}</td>
+            <td scope="row">{{ financialRatio[index].quickratio }}</td>
+            <td scope="row">{{ financialRatio[index].debttoequity }}</td>
+            <td scope="row">{{ financialRatio[index].roe }}</td>
+            <td scope="row">{{ financialRatio[index].dividendyield }}</td>
+            <td scope="row">{{ financialRatio[index].pricetoearnings }}</td>
+            <td scope="row"><router-link v-bind:to="stock.get_absolute_url" class="button is-dark mt-4">View Chart</router-link></td>
           </tr>
         </tbody>
       </table>
@@ -57,6 +69,7 @@ export default {
   data() {
     return {
       stocks: [],
+      financialRatio:[],
       selectedStocks: [],
       options: new Set([""]),
       value: ''
@@ -71,11 +84,14 @@ export default {
     async getStocks() {
       await axios
         .get('api/stocks')
-        .then(response => {
-          this.stocks = response.data,
-            console.log(response.data)
-            const Category = response.data.map(Category => Category.Category);
+        .then(response1 => {
+          return axios.get('api/financial-ratio')
+          .then(response2 => {
+            this.stocks = response1.data,
+            this.financialRatio = response2.data
+            const Category = response1.data.map(Category => Category.Category);
             Category.forEach(Category => this.options.add(Category));
+            })
         })
         .catch(error => {
           console.log(error)
@@ -85,11 +101,14 @@ export default {
     async filterStock(){
       await axios
         .get(`api/stocks/${this.value}`)
-        .then(response => {
-          this.stocks = response.data,
-            console.log("this.stocks", this.stocks)
-            const symbols = response.data.map(symbol => symbol.Symbol);
-            this.selectedStocks = symbols
+        .then(response1 => {
+          return axios.get(`api/stocks/${this.value}/financial-ratio`)
+            .then(response2 => {
+              this.stocks = response1.data,
+              this.financialRatio = response2.data
+              const symbols = response1.data.map(symbol => symbol.Symbol);
+              this.selectedStocks = symbols
+            })
         })
         .catch(error => {
           console.log(error)
