a665ec432a09b156089c2469f20c76357fae17a8
shengann <shengann2412@gmail.com>
2023-03-30 17:35:25 +0800

    delete serializer realted to us stocks

--- StockProf_Services/StockProf_app/api/serializer.py
+++ StockProf_Services/StockProf_app/api/serializer.py
@@ -1,28 +1,6 @@
 from rest_framework import serializers
 from StockProf_app.models import stock, financialRatios, MY_financialRatios, MY_stock, MY_stockPrice
 
-class finacialRatiosSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = financialRatios
-        # dataDate = serializers.DateTimeField(format="%Y-%m-%d")
-        fields = '__all__'
-        
-        
-class stockSerializer(serializers.ModelSerializer):
-    # financialRatios = finacialRatiosSerializer(many=True)
-    class Meta:
-        model = stock
-        fields = (
-            "id",
-            "Symbol",
-            "Name",
-            "get_absolute_url",
-            "Exchange",
-            "Sector",
-            "Industry",
-        )
-        
-
 class MY_finacial_ratiosSerializer(serializers.ModelSerializer):
     class Meta:
         model = MY_financialRatios
