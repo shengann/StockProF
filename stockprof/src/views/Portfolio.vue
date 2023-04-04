7d0a3b5227f23e16417d97d9085feb24e314cf68
shengann <shengann2412@gmail.com>
2023-04-04 10:47:23 +0800

    feature/boxplot

--- stockprof/src/views/Portfolio.vue
+++ stockprof/src/views/Portfolio.vue
@@ -20,12 +20,14 @@
                     </div>
                 </ul>
             </div>
-        </div>    
+        </div>
+        <div id="plot"></div> 
     </div>
 </template>
 
 <script>
 import axios from "axios"
+import * as d3 from 'd3';
 export default {
     name: 'Portfolio',
     props: ['selectedStocks'],
@@ -35,7 +37,8 @@ export default {
             clusteredStocks: [],
             outlierStocks: [],
             clusteredStocksSymbols :[], 
-            outlierStocksSymbols : []
+            outlierStocksSymbols : [],
+            boxPlotData: []
         }
     },
     mounted() {
@@ -53,15 +56,14 @@ export default {
                 .then(response => {
                     this.clusteredStocks = response.data.portfolio
                     this.outlierStocks = response.data.outlier
-                    console.log(this.clusteredStocks)
                     for (let i=0;i< response.data.portfolio.length;i++) {
                         const clustered_symbols = response.data.portfolio[i].map(symbol => symbol.Symbol);
                         this.clusteredStocksSymbols.push(clustered_symbols)
-
                     }
-                    // this.clusteredStocksSymbols = clustered_symbols
+                    console.log("this.clusteredStocksSymbols", this.clusteredStocksSymbols)
                     const outlier_symbols = response.data.outlier.map(symbol => symbol.Symbol);
                     this.outlierStocksSymbols = outlier_symbols
+                    this.getBoxPlotData()
                 })
                 .catch(error => {
                     console.log(error)
@@ -84,8 +86,104 @@ export default {
                 }
                 )
         },
+        async getBoxPlotData() {
+            await axios
+                .post('api/portfolio/box-plot-data',{
+                    "portfolio_list": this.clusteredStocksSymbols
+                }) 
+                .then(response => {
+                    this.boxPlotData = response.data
+
+                    const margin = { top: 10, right: 30, bottom: 30, left: 40 };
+                    const width = 1600 - margin.left - margin.right;
+                    const height = 400 - margin.top - margin.bottom;
+
+                    // append the svg object to the selected DOM element
+                    const svg = d3.select("#plot")
+                        .append('svg')
+                        .attr('width', width + margin.left + margin.right)
+                        .attr('height', height + margin.top + margin.bottom)
+                        .append('g')
+                        .attr('transform', `translate(${margin.left},${margin.top})`);
+
+                    // Compute quartiles, median, inter quantile range min and max
+                    const sumstat = this.boxPlotData.map((d) => {
+                        const q1 = d.q1;
+                        const median = d.q2;
+                        const q3 = d.q3;
+                        const interQuantileRange = d.iqr;
+                        const min = q1 - 1.5 * interQuantileRange;
+                        const max = q3 + 1.5 * interQuantileRange;
+                        return ({ name: d.name, q1, median, q3, interQuantileRange, min, max });
+                    });
+                    
+                    // Show the X scale
+                    const x = d3.scaleBand()
+                        .range([0, width])
+                        .domain(this.boxPlotData.map((d) => d.name))
+                        .paddingInner(1)
+                        .paddingOuter(0.5);
+                    svg.append('g')
+                        .attr('transform', `translate(0,${height})`)
+                        .call(d3.axisBottom(x));
+
+                    // Show the Y scale
+                    const y = d3.scaleLinear()
+                        .domain([-0.5, 1])
+                        .range([height, 0]);
+                    svg.append('g').call(d3.axisLeft(y));
+
+                    // Show the main vertical line
+                    svg
+                        .selectAll('vertLines')
+                        .data(sumstat)
+                        .enter()
+                        .append('line')
+                        .attr('x1', (d) => x(d.name))
+                        .attr('x2', (d) => x(d.name))
+                        .attr('y1', (d) => y(d.min))
+                        .attr('y2', (d) => y(d.max))
+                        .attr('stroke', 'black')
+                        .style('width', 40);
+
+                    // rectangle for the main box
+                    const boxWidth = 50;
+                    svg
+                        .selectAll('boxes')
+                        .data(sumstat)
+                        .enter()
+                        .append('rect')
+                        .attr('x', (d) => x(d.name) - boxWidth / 2)
+                        .attr('y', (d) => y(d.q3))
+                        .attr('height', (d) => y(d.q1) - y(d.q3))
+                        .attr('width', boxWidth)
+                        .attr('stroke', 'black')
+                        .style('fill', '#69b3a2');
+
+                    // Show the median
+                    svg
+                        .selectAll('medianLines')
+                        .data(sumstat)
+                        .enter()
+                        .append('line')
+                        .attr('x1', (d) => x(d.name) - boxWidth / 2)
+                        .attr('x2', (d) => x(d.name) + boxWidth/2)
+                        .attr("y1", function (d) { return (y(d.median)) }  )
+                        .attr("y2", function (d) { return (y(d.median)) }  )
+                        .attr("stroke", "black")
+                        .style("width", 80)
+                })
+                        
+                    
+                
+                .catch(error => {
+                    console.log(error)
+                }
+                )
+        }
     }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>
