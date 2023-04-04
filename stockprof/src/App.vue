ced9689f4d3dc72b6745064c7f0366292fceaa56
shengann <shengann2412@gmail.com>
2023-04-04 10:47:56 +0800

    nav bar for login and signup

--- stockprof/src/App.vue
+++ stockprof/src/App.vue
@@ -2,7 +2,9 @@
   <div id="wrapper">
     <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/about">Personal Profile</router-link> |
     </nav>
     <section class="section">
           <router-view />
