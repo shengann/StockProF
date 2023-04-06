dcb0bd3f07b5a888a20a180a79cb122dc743b644
shengann <shengann2412@gmail.com>
2023-04-06 17:53:50 +0800

    added stock type selection

--- stockprof/src/views/Portfolio.vue
+++ stockprof/src/views/Portfolio.vue
@@ -1,62 +1,69 @@
 <template>
-
-    <div class="page-portfolio">   
-        <h1 class="title">Sector : {{category}}</h1>
-        <div v-if="outlierStocks.length>1" class="box mt-6">
+    <div class="page-portfolio">
+        <h1 class="title">Sector : {{ category }}</h1>
+        <div v-if="outlierStocks.length > 1" class="box mt-6">
             <h2 class="title">Outlier Stocks</h2>
 
             <table class="table table-striped table-bordered table-sm">
-            <thead>
-              <tr>
-                <th class="text-center" scope="col">Name</th>
-                <th class="text-center" scope="col">Code</th>
-                <th class="text-center" scope="col">Total asset turnover</th>
-                <th class="text-center" scope="col">Cash ratio</th>
-                <th class="text-center" scope="col">Debt ratio</th>
-                <th class="text-center" scope="col">Return on equity </th>
-                <th class="text-center" scope="col">Dividend yield</th>
-                <th class="text-center" scope="col">Price earnings ratio </th>
-                <th class="text-center" scope="col">Capital Gain/Loss </th>
-
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="(financialRatio, index) in outlierFinancialratio" :key="index">
-                <td scope="row">{{ this.outlierStocks[index].Name }}</td>
-                <td scope="row">{{ this.outlierStocks[index].Symbol }}</td>
-                <td scope="row">{{ financialRatio[0].assetturnover }}</td>
-                <td scope="row">{{ financialRatio[0].quickratio }}</td>
-                <td scope="row">{{ financialRatio[0].debttoequity }}</td>
-                <td scope="row">{{ financialRatio[0].roe }}</td>
-                <td scope="row">{{ financialRatio[0].dividendyield }}</td>
-                <td scope="row">{{ financialRatio[0].pricetoearnings }}</td>
-                <td scope="row"></td>
-              </tr>
-            </tbody>
-          </table>
+                <thead>
+                    <tr>
+                        <th class="text-center" scope="col">Name</th>
+                        <th class="text-center" scope="col">Code</th>
+                        <th class="text-center" scope="col">Total asset turnover</th>
+                        <th class="text-center" scope="col">Cash ratio</th>
+                        <th class="text-center" scope="col">Debt ratio</th>
+                        <th class="text-center" scope="col">Return on equity </th>
+                        <th class="text-center" scope="col">Dividend yield</th>
+                        <th class="text-center" scope="col">Price earnings ratio </th>
+                        <th class="text-center" scope="col">Capital Gain/Loss </th>
+                        <th class="text-center" scope="col">Stock Type</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(financialRatio, index) in outlierFinancialratio" :key="index">
+                        <td scope="row">{{ this.outlierStocks[index].Name }}</td>
+                        <td scope="row">{{ this.outlierStocks[index].Symbol }}</td>
+                        <td scope="row">{{ financialRatio[0].assetturnover }}</td>
+                        <td scope="row">{{ financialRatio[0].quickratio }}</td>
+                        <td scope="row">{{ financialRatio[0].debttoequity }}</td>
+                        <td scope="row">{{ financialRatio[0].roe }}</td>
+                        <td scope="row">{{ financialRatio[0].dividendyield }}</td>
+                        <td scope="row">{{ financialRatio[0].pricetoearnings }}</td>
+                        <td scope="row">{{ financialRatio[0].pricetoearnings }}</td>
+                        <td scope="row">
+                            <div class="select is-small mb-3 mr-3">
+                                <select v-model="stockTypeOptions[index]" @change="showOutlierInput(index)" >
+                                    <option value="outperforming">Outpeforming</option>
+                                    <option value="underperforming">Underperforming</option>
+                                    <option value="custom">Custom</option>
+                                </select>
+                            </div>
+                            <input  v-if="showOutlierTextInput[index]"  class="input is-small" type="text" style="width:120px;" v-model="stockTypeOptions[index]" />
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
 
         <box-plot v-if="boxPlotData.length" :box-plot-data="boxPlotData"></box-plot>
 
         <div class="columns box">
-            <div class="column is-one-third"
-            v-for="(stockList,index) in clusteredStocks"
-            v-bind:key="stockList.id">
+            <div class="column is-one-third" v-for="(stockList, index) in clusteredStocks" v-bind:key="stockList.id">
                 <div>
-                    <h2 class="title">Portfolio {{index+1}}</h2>
+                    <h2 class="title">Portfolio {{ index + 1 }}</h2>
                     <table class="table table-striped table-bordered table-sm">
-                    <thead>
-                    <tr>
-                        <th class="text-center" scope="col">Name</th>
-                        <th class="text-center" scope="col">Code</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr v-for="stock in stockList" v-bind:key="stock.id">
-                        <td scope="row">{{ stock.Name }}</td>
-                        <td scope="row">{{ stock.Symbol }}</td>
-                    </tr>
-                    </tbody>
+                        <thead>
+                            <tr>
+                                <th class="text-center" scope="col">Name</th>
+                                <th class="text-center" scope="col">Code</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="stock in stockList" v-bind:key="stock.id">
+                                <td scope="row">{{ stock.Name }}</td>
+                                <td scope="row">{{ stock.Symbol }}</td>
+                            </tr>
+                        </tbody>
                     </table>
                 </div>
                 <div class="field">
@@ -72,18 +79,14 @@
                         </div>
                     </div>
                 </div>
-                <input 
-                    v-if="showTextInput[index]"
-                    v-model="portfolioTypeOptions[index]" 
-                    class="input my-4" type="text" 
-                    placeholder="Portfolio type"
-                >
-            
+                <input v-if="showTextInput[index]" v-model="portfolioTypeOptions[index]" class="input my-4" type="text"
+                    placeholder="Portfolio type">
+
             </div>
         </div>
-        <button  type="button"  @click="saveResult()" class="btn btn-primary mt-6">Save Results</button> 
+        <button type="button" @click="saveResult()" class="btn btn-primary mt-6">Save Results</button>
 
-    </div> 
+    </div>
 </template>
 
 <script>
@@ -98,11 +101,13 @@ export default {
             clusteredStocks: [],
             outlierStocks: [],
             outlierFinancialratio: [],
-            clusteredStocksSymbols :[], 
-            outlierStocksSymbols : [],
+            clusteredStocksSymbols: [],
+            outlierStocksSymbols: [],
             boxPlotData: [],
             portfolioTypeOptions: [],
-            showTextInput: []
+            showTextInput: [],
+            showOutlierTextInput: [],
+            stockTypeOptions: []
         }
     },
     components: {
@@ -114,28 +119,28 @@ export default {
         document.title = 'Portfolio' + ' | StockProF'
     },
     methods: {
-        async saveResult(){
-            console.log("this.portfolioTypeOptions",this.portfolioTypeOptions)
+        async saveResult() {
+            console.log("this.portfolioTypeOptions", this.portfolioTypeOptions)
             const data = {
                 'clusteredStocksSymbols': this.clusteredStocksSymbols,
                 'outlierStocksSymbols': this.outlierStocksSymbols,
                 'portfolioTypeOptions': this.portfolioTypeOptions,
-                'category':this.category
+                'category': this.category
             }
             await axios
-                .post('api/save-result',{data})
-                .then( response =>{
+                .post('api/save-result', { data })
+                .then(response => {
                     conosle.log(response)
                     this.$router.push('/profile')
                 })
-                .catch( error =>{
-                    console.log("error",error)
+                .catch(error => {
+                    console.log("error", error)
                 })
         },
         showInput(index) {
             if (this.portfolioTypeOptions[index] !== 'Aggressive' && this.portfolioTypeOptions[index] !== 'Average' && this.portfolioTypeOptions[index] !== 'Defensive'){
                 this.showTextInput[index] = true;
-                if (this.portfolioTypeOptions[index] == 'Custom'){
+                if (this.portfolioTypeOptions[index] == 'Custom') {
                     this.portfolioTypeOptions[index] = ''
                 }
             }
@@ -143,20 +148,34 @@ export default {
                 this.showTextInput[index] = false;
             }
         },
+        showOutlierInput(index) {
+            if (this.stockTypeOptions[index] !== 'outperforming' && this.stockTypeOptions[index] !== 'underperforming') {
+                this.showOutlierTextInput[index] = true;
+                if (this.stockTypeOptions[index] == 'custom') {
+                    this.stockTypeOptions[index] = ''
+                }
+            }
+            else {
+                this.showOutlierTextInput[index] = false;
+            }
+        },
         async getPortfolio() {
             await axios
-                .post('api/stockprof',{
-                    "ticker_list":this.stocks,
+                .post('api/stockprof', {
+                    "ticker_list": this.stocks,
                 })
                 .then(response => {
                     this.clusteredStocks = response.data.portfolio
                     this.outlierStocks = response.data.outlier
                     const outlierFinancialratio = response.data.outlier.map(financial_ratios => financial_ratios.financial_ratios);
                     this.outlierFinancialratio = outlierFinancialratio
-                    for (let i=0;i< response.data.portfolio.length;i++) {
+                    for (let i = 0; i < response.data.portfolio.length; i++) {
                         const clustered_symbols = response.data.portfolio[i].map(symbol => symbol.Symbol);
                         this.clusteredStocksSymbols.push(clustered_symbols)
                         this.showTextInput.push(false)
+                    }
+                    for (let j = 0; j < response.data.outlier.length; j++ ){
+                        this.showOutlierTextInput.push(false)
                     }
                     const outlier_symbols = response.data.outlier.map(symbol => symbol.Symbol);
                     this.outlierStocksSymbols = outlier_symbols
@@ -187,9 +206,9 @@ export default {
         },
         async getBoxPlotData() {
             await axios
-                .post('api/portfolio/box-plot-data',{
+                .post('api/portfolio/box-plot-data', {
                     "portfolio_list": this.clusteredStocksSymbols
-                }) 
+                })
                 .then(response => {
                     this.boxPlotData = response.data
                 })
@@ -202,5 +221,4 @@ export default {
 }
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
