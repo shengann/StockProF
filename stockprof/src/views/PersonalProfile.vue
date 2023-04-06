eb80a4c7ffe79dd119a1600c907837e159e82bfd
shengann <shengann2412@gmail.com>
2023-04-06 10:00:03 +0800

    feature/disply the hsitory table

--- stockprof/src/views/PersonalProfile.vue
+++ stockprof/src/views/PersonalProfile.vue
@@ -1,34 +1,52 @@
 <template>
     <div class="page-my-account">
-        <div class="columns is-multiline">
-            <div class="column is-12">
-                <h1 class="title">My account</h1>
-            </div>
-
-            <div class="column is-12">
-                <button @click="logout()" class="button is-danger">Log out</button>
-            </div>
-
-            <hr>
-
-            <div class="column is-12">
-                <h2 class="subtitle">My orders</h2>
-
-            </div>
+        <div class="column is-12">
+            <h1 class="title">My account</h1>
         </div>
+
+        <div class="column is-12">
+            <button @click="logout()" class="button is-danger">Log out</button>
+        </div>
+
+        <div class="box">
+            <table class="table table-striped table-bordered table-sm">
+                <thead>
+                    <tr>
+                        <th class="text-center" scope="col">Category</th>
+                        <th class="text-center" scope="col">Date Created</th>
+                        <th class="text-center" scope="col">remarks</th>
+                        <th class="text-center" scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="data in history" :key="data.id">
+                        <td scope="row">{{ data.category}}</td>
+                        <td scope="row">{{ formatDate(data.date_created)}}</td>
+                        <td>idk</td>
+                        <td>View Chart</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
     </div>
 </template>
 
 <script>
 import axios from 'axios'
+import moment from 'moment';
+
 export default {
     name: 'PersonalProfile',
     data() {
         return {
+            history:[]
         }
     },
     mounted() {
-        document.title = 'My account | Djackets'
+        document.title = 'My account | StockProF'
+
+        this.getHistory()
     },
     methods: {
         logout() {
@@ -38,7 +56,17 @@ export default {
             localStorage.removeItem("userid")
             this.$store.commit('removeToken')
             this.$router.push('/')
-        }
+        },
+        async getHistory() {
+            await axios
+                .get('api/history')
+                .then(response => {
+                    this.history = response.data
+                })
+        },
+        formatDate(value) {
+            return moment(value, 'YYYY-MM-DDTHH:mm:ss.SSSZZ').local().format('MMM D, YYYY h:mm A');
+        },
     }
 }
 </script>
