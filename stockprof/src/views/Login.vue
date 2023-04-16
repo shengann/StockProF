cd6c2bb2418af3c23471799afec2f867c26fdae9
shengann <shengann2412@gmail.com>
2023-04-16 11:25:00 +0800

    redeisgn login page ui

--- stockprof/src/views/Login.vue
+++ stockprof/src/views/Login.vue
@@ -1,40 +1,69 @@
 <template>
     <div class="page-log-in">
-        <div class="columns">
-            <div class="column is-4 is-offset-4">
-                <h1 class="title">Log in</h1>
+        <section class="h-100 gradient-form" style="background-color: #eee;">
+            <div class="container py-5 h-100">
+                <div class="row d-flex justify-content-center align-items-center h-100">
+                    <div class="col-xl-10">
+                        <div class="card rounded-3 text-black">
+                            <div class="row g-0">
+                                <div class="col-lg-6">
+                                    <div class="card-body p-md-5 mx-md-4">
 
-                <form @submit.prevent="submitForm">
-                    <div class="field">
-                        <label>Username</label>
-                        <div class="control">
-                            <input type="text" class="input" v-model="username">
+                                        <div class="text-center">
+                                            <img img src="../assets/StockProF.png" alt="StockProF" style="width: 185px;">
+                                        </div>
+
+                                        <form @submit.prevent="submitForm">
+                                            <p class="title">Log in</p>
+
+
+                                            <div class="field">
+                                                <label>Username</label>
+                                                <div class="control">
+                                                    <input type="text" class="input" v-model="username">
+                                                </div>
+                                            </div>
+
+                                            <div class="field">
+                                                <label>Password</label>
+                                                <div class="control">
+                                                    <input type="password" class="input" v-model="password">
+                                                </div>
+                                            </div>
+
+                                            <div class="notification is-danger" v-if="errors.length">
+                                                <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
+                                            </div>
+
+                                            <div class="field">
+                                                <div class="control">
+                                                    <button class="button is-dark">Log in</button>
+                                                </div>
+                                            </div>
+
+
+                                            Or <router-link class="mt-5" to="/signup">click here</router-link> to sign up!
+
+                                        </form>
+
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+                                    <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                                        <h4 class="mb-4">StockProF (Stock Profilling Framework)</h4>
+                                        <p class="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                                            do eiusmod
+                                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                            quis nostrud
+                                            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-
-                    <div class="field">
-                        <label>Password</label>
-                        <div class="control">
-                            <input type="password" class="input" v-model="password">
-                        </div>
-                    </div>
-
-                    <div class="notification is-danger" v-if="errors.length">
-                        <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
-                    </div>
-
-                    <div class="field">
-                        <div class="control">
-                            <button class="button is-dark">Log in</button>
-                        </div>
-                    </div>
-
-                    <hr>
-
-                    Or <router-link to="/sign-up">click here</router-link> to sign up!
-                </form>
+                </div>
             </div>
-        </div>
+        </section>
     </div>
 </template>
 
@@ -51,7 +80,7 @@ export default {
         }
     },
     mounted() {
-        document.title = 'Log In | Djackets'
+        document.title = 'Log In | StockProF'
     },
     methods: {
         async submitForm() {
@@ -94,3 +123,29 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+.gradient-custom-2 {
+    /* fallback for old browsers */
+    background: #fccb90;
+
+    /* Chrome 10-25, Safari 5.1-6 */
+    background: -webkit-linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593);
+
+    /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    background: linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593);
+}
+
+@media (min-width: 768px) {
+    .gradient-form {
+        height: 100vh !important;
+    }
+}
+
+@media (min-width: 769px) {
+    .gradient-custom-2 {
+        border-top-right-radius: .3rem;
+        border-bottom-right-radius: .3rem;
+    }
+}
+</style>
