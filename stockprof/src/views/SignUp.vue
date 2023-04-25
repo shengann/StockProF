791b5ae89168684a1658201f8fc955c0d2a4ed83
shengann <shengann2412@gmail.com>
2023-04-25 08:26:26 +0800

    added stockprof desc

--- stockprof/src/views/SignUp.vue
+++ stockprof/src/views/SignUp.vue
@@ -65,11 +65,9 @@
                                 <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
                                     <div class="text-white px-3 py-4 p-md-5 mx-md-4">
                                         <h4 class="mb-4">StockProF (Stock Profilling Framework)</h4>
-                                        <p class="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                                            do eiusmod
-                                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                            quis nostrud
-                                            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                        <p class="small mb-0">                                            
+                                            StockProF is a stock profiling framework that enables you to quickly build a portfolio from selected sectors in Bursa Malaysia, saving you time in selecting individual stocks. With StockProF, you can profile the stocks and portdolio generated based on the portfolio profile of each portfolio.</p>
+
                                     </div>
                                 </div>
                             </div>
@@ -126,7 +124,7 @@ export default {
                             type: 'is-success',
                             dismissible: true,
                             pauseOnHover: true,
-                            duration: 2000,
+                            duration: 4000,
                             position: 'bottom-right',
                         })
 
