3b9085da381b31aadc310c04152f0518b0e42add
shengann <shengann2412@gmail.com>
2023-05-05 21:06:50 +0800

    change backend api link

--- StockProf_Services/StockProf_app/api/urls.py
+++ StockProf_Services/StockProf_app/api/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from StockProf_app.api.views import getStockPriceData, getStockProfData, stockList, filterStock, MY_getFinancialRatiosData, MY_getStockPrice, MY_getComparison, filterFinancialRatio, getBoxPlotData, getPastPortfolioData
+from StockProf_app.api.views import getStockPriceData, getStockProFData, stockList, filterStock, MY_getFinancialRatiosData, MY_getStockPrice, MY_getComparison, filterFinancialRatio, getBoxPlotData, getPastPortfolioData
 
 router = DefaultRouter()
 router.register("stocks",stockList,basename="stocks")
 urlpatterns = [
     path('',include(router.urls)),
-    path('stocks/<str:Category>', filterStock.as_view(), name='filterStock'),
+    path('stocks-category/<str:Category>', filterStock.as_view(), name='filterStock'),
     path('stocks/<str:Category>/financial-ratio', filterFinancialRatio.as_view(),name='filterFinancialRatio'),
     path('stock/<slug:ticker>', getStockPriceData.as_view(), name='get-Stock-Data'),
-    path('stockprof', getStockProfData.as_view(), name='getStockProfData'),
+    path('stockprof', getStockProFData.as_view(), name='getStockProFData'),
     path('financial-ratio', MY_getFinancialRatiosData.as_view(),name='MY_getFinancialRatiosData'),
     path('stock-price', MY_getStockPrice.as_view(), name='stock-price'),
     path('comparison', MY_getComparison.as_view(), name='comparison'),
